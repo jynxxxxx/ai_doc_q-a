1c4e631be10d5f9ec582fde3c9f28a04450094bc
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-09-02 21:54:24 +0900

    add upload documents button to document management

--- frontend/src/components/DocUpload.tsx
+++ frontend/src/components/DocUpload.tsx
@@ -75,7 +75,7 @@ export default function DocumentUpload({ onUploadComplete }: DocumentUploadProps
   };
 
   return (
-    <div className="mb-4 h-full">
+    <div className="mb-4 h-full w-full">
       {selectedFiles.length > 0 && (
         <>
           <div className="mt-6 space-y-3 grid grid-cols-3 gap-4 max-w-3xl mx-auto">
--- frontend/src/pages/DocPage.tsx
+++ frontend/src/pages/DocPage.tsx
@@ -3,10 +3,11 @@ import DocUpload from "../components/DocUpload";
 import type { Document } from "../types";
 import { deleteDocument, getDocuments, getDocumentURL } from "../apis";
 import { toast } from "sonner";
-import { FileText, Eye, Trash2 } from 'lucide-react';
+import { FileText, Eye, Trash2, Plus, X } from 'lucide-react';
 
 export default function DocPage() {
   const [documents, setDocuments] = useState<Document[]>([]);
+  const [openUpload, setOpenUpload] = useState(false);
 
   useEffect(() => {
     const fetchDocuments = async () => {
@@ -37,18 +38,32 @@ export default function DocPage() {
 
   return (
     <div className="p-12">
-      <h2 className="text-left text-xl font-bold ml-12 mb-4 ">Manage Documents</h2>
+      <div className="relative w-full">
+        <h2 className="text-left text-xl font-bold ml-12 mb-4 ">Manage Documents</h2>
+        <div 
+          className="absolute top-0 right-0 w-fit text-xs flex justify-center items-center gap-1 rounded-lg border p-2 bg-white text-gray-700 hover:text-gray-500 hover:border-gray-500 hover:cursor-pointer"
+          onClick={()=> setOpenUpload(true)}
+        >
+          <Plus className="w-4 h-4" />
+          <span className="">Upload Documents</span>
+        </div>
+      </div>
+      
       <div className="h-pt border border-b mb-12"></div>
       {!documents || documents.length === 0 ? (
         <>
           <div className="text-center">You have no documents uploaded yet. Upload PDFs to get started.</div>            
           <div className="mt-4 mx-auto h-48">
-            <DocUpload onUploadComplete={(newDocs: any) => setDocuments([...documents, ...newDocs])} />
+            <DocUpload
+              onUploadComplete={(newDocs: Document[]) => {
+                setDocuments([...documents, ...newDocs]);
+                setOpenUpload(false);
+              }}
+            />
           </div>
         </>
       ) : (
         <>
-          <h3 className="font-bold mb-2">Document Library</h3>
           <div className="grid grid-cols-4 gap-4 mx-auto mb-6">
             {documents.map((doc) => (
               <div key={doc.id} className="border px-2 pt-4 pb-2 rounded h-full flex flex-col gap-4 items-center justify-between">
@@ -74,6 +89,25 @@ export default function DocPage() {
             ))}
           </div>
         </>
+      )}
+
+      {openUpload && (
+        <div className="fixed inset-0 bg-black/30 flex justify-center items-start pt-20 z-50 w-full">
+          <div className="relative mt-4 h-48 bg-white w-3/5 h-[60vh] p-6 rounded-lg shadow-lg flex justify-center items-center flex-col">
+            <button
+              onClick={() => setOpenUpload(false)}
+              className="absolute right-2 top-2 mt-auto self-end text-gray-500 hover:text-gray-700"
+            >
+              <X className="w-4 h-4" />
+            </button>
+            <DocUpload
+              onUploadComplete={(newDocs: Document[]) => {
+                setDocuments([...documents, ...newDocs]);
+                setOpenUpload(false);
+              }}
+            />
+          </div>
+        </div>
       )}
     </div>
   );
