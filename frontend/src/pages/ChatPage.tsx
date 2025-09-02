97eb0a1832f923ba44a5881085323cf3391db75b
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-09-02 22:04:53 +0900

    add upload document button to Chat page

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import Chat from '../components/Chat'
-import DocumentUpload from '../components/DocUpload'
+import DocUpload from '../components/DocUpload'
 import { getDocuments } from "../apis";
 import type { Document } from "../types";
+import { FileText, Eye, Trash2, Plus, X } from 'lucide-react';
+
 
 function ChatPage() {
   const [documents, setDocuments] = useState<Document[]>([]);
+  const [openUpload, setOpenUpload] = useState(false);
 
    useEffect(() => {
       const fetchDocuments = async () => {
@@ -17,10 +20,24 @@ function ChatPage() {
     }, []);
 
   return (
-    <>
+    <div className="p-12">
+      <div className="flex items-end gap-2">
+        <h2 className="text-left text-xl font-bold ml-12 mb-4 ">AI Chat</h2>
+        <span className="text-sm text-gray-500 mb-5">- Chat with your documents</span>
+      </div>
+      <div className="h-pt border border-b mb-6"></div>
       <Chat />
-      <div className="my-4">
-        RAG documents:
+      <div className="my-4 max-w-4xl mx-auto">
+        <div className="relative mb-4">
+          RAG documents:
+          <div 
+            className="absolute top-0 right-0 w-fit text-xs flex justify-center items-center gap-1 rounded-lg border p-2 bg-white text-gray-700 hover:text-gray-500 hover:border-gray-500 hover:cursor-pointer"
+            onClick={()=> setOpenUpload(true)}
+          >
+            <Plus className="w-4 h-4" />
+            <span className="">Upload Documents</span>
+          </div>
+        </div>
         {!documents || documents.length === 0 ? (
           <p>No documents uploaded yet.</p>
         ) : (
@@ -34,8 +51,25 @@ function ChatPage() {
         )}
       </div>
       
-      <DocumentUpload onUploadComplete={(newDocs: any) => setDocuments([...documents, ...newDocs])}/>
-    </>
+      {openUpload && (
+        <div className="fixed inset-0 bg-black/30 flex justify-center items-start pt-20 z-50 w-full">
+          <div className="relative mt-4 h-48 bg-white w-3/5 h-[60vh] p-6 rounded-lg shadow-lg flex justify-center items-center flex-col">
+            <button
+              onClick={() => setOpenUpload(false)}
+              className="absolute right-2 top-2 mt-auto self-end text-gray-500 hover:text-gray-700"
+            >
+              <X className="w-4 h-4" />
+            </button>
+            <DocUpload
+              onUploadComplete={(newDocs: Document[]) => {
+                setDocuments([...documents, ...newDocs]);
+                setOpenUpload(false);
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
